fef3eb558eae8f7ab3810b4e39d8422f7a2f1913
Ahnaf19 <ahnaftanjid19@gmail.com>
2025-02-13 18:37:08 +0600

    saved

---
+++ app/routers/model_router.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+
+from app.schemas.model_schema import GuestBase, GuestData, UpdateGuest
+from app.services.guest_service import GuestService
+
+# Initialize the router with a prefix and tags
+router = APIRouter(prefix="/model", tags=["model"])
+
+# Initiate the GuestService with dummy data
+guest_service = GuestService(dummy_guest_data)
+
+
+# Define the routes
+@router.get("/health", response_model=GuestData)
+async def read_guests() -> GuestData:
+    """
+    Retrieve all guests.
+
+    Returns:
+        GuestData: A list of all guests.
+    """
+    return guest_service.dummy_guest_data
+
